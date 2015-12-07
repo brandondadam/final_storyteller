adbb58839da20c9ec6db63866dff6bdb4cf4d58c
bdadam <bdadam@mail.bradley.edu>
2015-12-07 17:42:33 -0600

    formatted stuff, just have to make the directory in the terminal

--- index.php
+++ index.php
@@ -10,19 +10,16 @@
 		if (!file_exists($_GET['msg'])) {
 		  mkdir($_GET['msg']);
 		}
-
-			if(!empty($_GET['msg'])){
-				echo $_GET['msg'];
-				$filename = time() . '.txt';
-				file_put_contents("msg/$filename", $_GET['msg']);
-
+		if(!empty($_GET['msg'])){
+			echo $_GET['msg'];
+			$filename = time() . '.txt';
+			file_put_contents("msg/$filename", $_GET['msg']);
 		}
-
 
 		$msgs=glob('msg/*.txt');
 
 		?>
-
+	
 		<form action="./">
 			<input type="text" name="msg" placeholder="Add to the story...">
 			<input type="submit" value="Send">
