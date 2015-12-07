f359c18a9978be3d05fa36d74f0ccbac66a19d05
bdadam <bdadam@mail.bradley.edu>
2015-12-07 16:18:45 -0600

    added php echo output

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 		<title>Co-Authors</title>
 	</head>
 	<body>
+		<?php
+
+		if(!empty($_GET['msg'])){
+			echo $_GET('msg');
+		}
+		
+		?>
 		<form action="./">
 			<input type="text" name="msg" placeholder="Add to the story...">
 			<input type="submit" value="send">
