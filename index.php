bb7f2fecdafcab8bdcf0d5e4fb4309c96cd81951
bdadam <bdadam@mail.bradley.edu>
2015-12-08 16:39:21 -0600

    added columns

--- index.php
+++ index.php
@@ -6,24 +6,27 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
+		<div class="container">
+			<div class="left">
+				<form action="submit.php" method="post">
+					<textarea type="text" id="type" name="msg" placeholder="Add to the story..." rows="8" cols"80"></textarea>
+					<input type="submit" value="Send">
+				</form>
+			</div>
 
-			<form action="submit.php" method="post">
-				<textarea type="text" id="type" name="msg" placeholder="Add to the story..." rows="8" cols"80"></textarea>
-				<input type="submit" value="Send">
-			</form>
-
-		<div id="msgs">
-			<?php
-				if (!file_exists('msg')) {
-					mkdir('msg');
-				}
-				$msgs=glob('msg/*.txt');
-				foreach ($msgs as $filename){
-					$msg = file_get_contents($filename);
-					echo '<p>' . htmlentities($msg) . '</p>';
-			}
-		?>
-	</div>
+			<div class="right" id="msgs">
+				<?php
+					if (!file_exists('msg')) {
+						mkdir('msg');
+					}
+					$msgs=glob('msg/*.txt');
+					foreach ($msgs as $filename){
+						$msg = file_get_contents($filename);
+						echo '<p>' . htmlentities($msg) . '</p>';
+					}
+				?>
+			</div>
+		</div>
 		<script src="jquery-1.11.3.min.js"></script>
 		<script src="script.js"></script>
 	</body>
