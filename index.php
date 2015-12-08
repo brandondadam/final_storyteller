2d930e9a27f85fa7d2f1c43f7a81529edadbb86f
bdadam <bdadam@mail.bradley.edu>
2015-12-07 19:13:53 -0600

    added .js and added serialize method and prevented default for adding msgs

--- index.php
+++ index.php
@@ -28,6 +28,8 @@
 			<input type="text" name="msg" placeholder="Add to the story...">
 			<input type="submit" value="Send">
 		</form>
+
 		<script src="jquery-1.11.3.min.js"></script>
+		<script src="script.js"></script>
 	</body>
 </html>
---
+++ script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+	$('form').submit(function(e)){
+		e.preventDefault();
+		$.get('./?')
+	});
+}
