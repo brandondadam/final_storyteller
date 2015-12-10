c18b7bbd6162d680f784fc6b37a32f0f65f2f107
bdadam <bdadam@mail.bradley.edu>
2015-12-09 18:25:48 -0600

    do the same thing as send button but with enter

--- script.js
+++ script.js
@@ -24,6 +24,28 @@ jQuery(document).ready(function($) {
 function checkSubmit(e){
 	var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
 	if(charCode == 13){
-		document.getElementById('storyForm').submit();
+		jQuery(document).ready(function($) {
+			$('form').submit(function(e) {
+				console.log('checking');
+				e.preventDefault();
+				$.ajax('submit.php', {
+					method: 'POST',
+					data : $('form').serialize(),
+					success: function(msgs) {
+						$('#msgs').html(msgs);
+					}
+				});
+			});
+
+			setInterval(function(){
+				$.ajax('update.php', {
+					method: 'POST',
+					success: function(msgs) {
+						$('#msgs').html(msgs);
+					}
+				});
+			}, 1000);
+
+		});
 	}
 }
