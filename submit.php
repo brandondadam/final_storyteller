1699e946b6e898ed3306c39dff3ec0a2dcdf7a19
bdadam <bdadam@mail.bradley.edu>
2015-12-10 03:07:56 -0600

    try this

--- submit.php
+++ submit.php
@@ -2,14 +2,16 @@
 if(!empty($_POST['msg'])){
 	$filename = time() . '.txt';
 	file_put_contents("msg/$filename", $_POST['msg']);
+
+	unset($_SESSION['form_token']);
+
 	$msgs=glob('msg/*.txt');
 	foreach ($msgs as $filename){
 		$msg = file_get_contents($filename);
 		echo '<p>' . htmlentities($msg) . '</p>';
 	}
 }
-$filename = filter_var($_POST['msg'], FILTER_SANITIZE_STRING);
+	$filename = filter_var($_POST['msg'], FILTER_SANITIZE_STRING);
 
-                /*** unset the form token in the session ***/
-                unset( $_SESSION['form_token']);
+
 ?>
