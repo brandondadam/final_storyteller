301fc08d6bc2894003bfa72f6bf18ef678e5a0e9
bdadam <bdadam@mail.bradley.edu>
2015-12-09 18:58:50 -0600

    putting it into js file now

--- script.js
+++ script.js
@@ -23,27 +23,9 @@ jQuery(document).ready(function($) {
 });
 
 
-/*function checkSubmit(e){
+function checkSubmit(e){
 	var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
 	if(charCode == 13){
-			$('form').submit(function(e) {
-				console.log('checking');
-				e.preventDefault();
-				$.ajax('submit.php', {
-					method: 'POST',
-					data : $('form').serialize(),
-					success: function(msgs) {
-						$('#msgs').html(msgs);
-					}
-				});
-			});
-			setInterval(function(){
-				$.ajax('update.php', {
-					method: 'POST',
-					success: function(msgs) {
-						$('#msgs').html(msgs);
-					}
-				});
-			}, 1000);
-		}
-	}*/
+		console.log('ckdle');
+	}
+}
