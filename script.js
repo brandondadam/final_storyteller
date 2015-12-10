bc979b9a662b4f51e73faf288cbafde1cfcd061f
bdadam <bdadam@mail.bradley.edu>
2015-12-09 18:32:37 -0600

    try this

--- script.js
+++ script.js
@@ -20,31 +20,31 @@ jQuery(document).ready(function($) {
 		});
 	}, 1000);
 
+
+	function checkSubmit(e){
+		var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
+		if(charCode == 13){
+
+				$('form').submit(function(e) {
+					console.log('checking');
+					e.preventDefault();
+					$.ajax('submit.php', {
+						method: 'POST',
+						data : $('form').serialize(),
+						success: function(msgs) {
+							$('#msgs').html(msgs);
+						}
+					});
+				});
+
+				setInterval(function(){
+					$.ajax('update.php', {
+						method: 'POST',
+						success: function(msgs) {
+							$('#msgs').html(msgs);
+						}
+					});
+				}, 1000);
+			}
+		}
 });
-function checkSubmit(e){
-	var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
-	if(charCode == 13){
-
-			$('form').submit(function(e) {
-				console.log('checking');
-				e.preventDefault();
-				$.ajax('submit.php', {
-					method: 'POST',
-					data : $('form').serialize(),
-					success: function(msgs) {
-						$('#msgs').html(msgs);
-					}
-				});
-			});
-
-			setInterval(function(){
-				$.ajax('update.php', {
-					method: 'POST',
-					success: function(msgs) {
-						$('#msgs').html(msgs);
-					}
-				});
-			}, 1000);
-
-		});
-	}
