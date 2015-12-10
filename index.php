009cc608b47a8720bf7b2e0875499a1fd8353f6a
bdadam <bdadam@mail.bradley.edu>
2015-12-10 03:37:00 -0600

    try this?

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-session_start();
+        /*** begin the session ***/
+        session_start();
 
+        /*** create the form token ***/
+        $form_token = uniqid();
 
-if(isset($_POST['token']){
-	echo "token";
-	if(isset($_SESSION['token']) && $_POST['token']==$_SESSION['token']){
-		$msg = htmlentities($_POST['msg']);
-	}
+if(isset($_POST['msg'], $_POST['msg'], $_SESSION['form_token'])){
+
+	/*** unset the form token in the session ***/
+	unset( $_SESSION['form_token']);
+
 }
 
 
-
-
-$token = $_SESSION['token'] = md5(uniqid(mt_rand(),true));
-
+        /*** add the form token to the session ***/
+        $_SESSION['form_token'] = $form_token;
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +30,9 @@ $token = $_SESSION['token'] = md5(uniqid(mt_rand(),true));
 		<form onKeyPress="return checkSubmit(event);">
 			<textarea type="text" id="type" name="msg" placeholder="Add to the story..." rows="8" cols"80"></textarea>
 
-			<input type="hidden" name="token" value="<?php=$token;?>" />
+
+			<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+
 
 		</form>
 
