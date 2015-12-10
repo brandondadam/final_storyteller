82756f9ce554383ea9c1e291140cc9cd7c9d8af4
bdadam <bdadam@mail.bradley.edu>
2015-12-09 18:37:44 -0600

    maybe

--- script.js
+++ script.js
@@ -1,50 +1,29 @@
+var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
+
 jQuery(document).ready(function($) {
 	$('form').submit(function(e) {
-		console.log('checking');
-		e.preventDefault();
-		$.ajax('submit.php', {
-			method: 'POST',
-			data : $('form').serialize(),
-			success: function(msgs) {
-				$('#msgs').html(msgs);
-			}
+		if(charCode==13){
+			console.log('checking');
+			e.preventDefault();
+			$.ajax('submit.php', {
+				method: 'POST',
+				data : $('form').serialize(),
+				success: function(msgs) {
+					$('#msgs').html(msgs);
+				}
+			});
 		});
-	});
 
-	setInterval(function(){
-		$.ajax('update.php', {
-			method: 'POST',
-			success: function(msgs) {
-				$('#msgs').html(msgs);
-			}
-		});
-	}, 1000);
+		setInterval(function(){
+			$.ajax('update.php', {
+				method: 'POST',
+				success: function(msgs) {
+					$('#msgs').html(msgs);
+				}
+			});
+		}, 1000);
+		}
 
 
-
-		var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
-		if(charCode == 13){
-
-				$('form').submit(function(e) {
-					console.log('checking');
-					e.preventDefault();
-					$.ajax('submit.php', {
-						method: 'POST',
-						data : $('form').serialize(),
-						success: function(msgs) {
-							$('#msgs').html(msgs);
-						}
-					});
-				});
-
-				setInterval(function(){
-					$.ajax('update.php', {
-						method: 'POST',
-						success: function(msgs) {
-							$('#msgs').html(msgs);
-						}
-					});
-				}, 1000);
-			}
 
 });
