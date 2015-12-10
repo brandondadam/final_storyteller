7c49dd261645d2575024e935d90b070a5ed7c630
bdadam <bdadam@mail.bradley.edu>
2015-12-09 19:21:20 -0600

    big oops

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
+console.log('ckdle');
+$('form').submit(function(e) {
+	console.log('checking');
+	e.preventDefault();
+	$.ajax('submit.php', {
+		method: 'POST',
+		data : $('form').serialize(),
+		success: function(msgs) {
+			$('#msgs').html(msgs);
+		}
+	});
+});
+
+setInterval(function() {
+	$.ajax('update.php', {
+		method: 'POST',
+		success: function(msgs) {
+			$('#msgs').html(msgs);
+		}
+	});
+}, 1000);
 
 
 function checkSubmit(e){
 	var charCode = e ? (e.which ? e.which: e.keycode): window.event.keycode;
 	if(charCode == 13){
-		console.log('ckdle');
-			$('form').submit(function(e) {
-				console.log('checking');
-				e.preventDefault();
-				$.ajax('submit.php', {
-					method: 'POST',
-					data : $('form').serialize(),
-					success: function(msgs) {
-						$('#msgs').html(msgs);
-					}
-				});
-			});
-
-			setInterval(function() {
-				$.ajax('update.php', {
-					method: 'POST',
-					success: function(msgs) {
-						$('#msgs').html(msgs);
-					}
-				});
-			}, 1000);
-		}
+		$('form').submit();
+	}
 }
