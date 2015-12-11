c001090df3365f176be19fe4cf7841ffa086b6e2
bdadam <bdadam@mail.bradley.edu>
2015-12-10 20:23:13 -0600

    try this

--- index.php
+++ index.php
@@ -9,52 +9,31 @@
 	<body>
 
 
-		<?php
+
+
 		session_start();
 		if (empty($_SESSION['already_submitted']) {
 
 			<form onKeyPress="return checkSubmit(event);">
 				<textarea type="text" id="type" name="msg" placeholder="what's on your mind..." rows="8" cols"80"></textarea>
 			</form>
+			<div id="msgs">
 
 		} else {
 		   echo 'You only get to submit once!';
 		}
-		?>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<div id="msgs">
 			<?php
 				if (!file_exists('msg')) {
 					mkdir('msg');
 				}
-				//condtional for clearing msgs, doesn't work
-				//if('msg' == 'reset'){
-					//foreach ($msgs as $filename){
-						//file_put_contents($filename, '');
-				//}
-
 				$msgs=glob('msg/*.txt');
 				foreach ($msgs as $filename){
 					$msg = file_get_contents($filename);
 					echo '<p>' . htmlentities($msg) . '</p>';
-					//this is for clearing messages, and it works, but then I can't submit anymore.
+					//this is for clearing messages
 					//file_put_contents($filename, '');
 				}
 			?>
