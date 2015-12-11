3107245be7eb1f992a4ad20d3897692746ae4d51
bdadam <bdadam@mail.bradley.edu>
2015-12-10 23:58:41 -0600

    try more time

--- index.php
+++ index.php
@@ -36,15 +36,25 @@
 			?>
 		</div>
 
-		<?php
-		$today = getdate();
-		$today = $today[yday];
+		<?PHP
+		$saveTime = (1234567890+3600); // Saved time from file/database
+		$thisTime = time(); // Current time
+		$diffTime = (saveTime-thisTime); // Difference in time
+
+		  if($diffTime >= 1) {
+		    $countMin = floor(diffTime/60);
+		    $countSec = (diffTime-(countMin*60));
+		    echo 'Time remaining until next run is in ',$countMin,' minute(s) ',$countSec,' seconds';
+		  } else {
+		    echo 'Timer expired.';
+		  }
 		?>
 
 		<div class="timer">
 		<p><?php
-			echo $today
+			echo 'Time remaining until next run is in ',$countMin,';
 
+echo idate('yday', time());
 			?></p>
 	</div>
 
