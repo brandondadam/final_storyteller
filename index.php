aca6627c75fb558410405ebc2d688947bf7e8781
bdadam <bdadam@mail.bradley.edu>
2015-12-11 00:09:21 -0600

    try this

--- index.php
+++ index.php
@@ -36,23 +36,13 @@
 			?>
 		</div>
 
-		<?PHP
-		$saveTime = (1234567890+3600); // Saved time from file/database
-		$thisTime = time(); // Current time
-		$diffTime = ($saveTime-$thisTime); // Difference in time
+		<?php
 
-		if($diffTime >= 1) {
-		$countMin = floor($diffTime/60);
-		$countSec = ($diffTime-($countMin*60));
-		echo 'Time remaining until next run is in ',$countMin,' minute(s) ',$countSec,' seconds';
-		  } else {
-		    echo 'Timer expired.';
-		  }
 		?>
 
 		<div class="timer">
 		<p><?php
-			echo 'Time remaining until next run is in ',$countMin,' minute(s) ',$countSec,' seconds';
+			echo idate('z');
 			?></p>
 	</div>
 
